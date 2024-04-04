43c77631ef0b0c468e7a4fc8ffe74b7291119595
taek2222 <oot309@daum.net>
2024-04-05 02:52:46 +0900

    [Oh] Settings

--- BackEnd/src/main/java/com/application/safety/controller/MemberController.java
+++ BackEnd/src/main/java/com/application/safety/controller/MemberController.java
@@ -1,23 +1,16 @@
 package com.application.safety.controller;
 
-import com.application.safety.dto.UserShowDto;
-import com.application.safety.entity.UserData;
-import com.application.safety.entity.UserInfo;
 import com.application.safety.entity.UserProfile;
 import com.application.safety.repository.UserDataRepository;
 import com.application.safety.repository.UserInfoRepository;
 import com.application.safety.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
 
 @RestController
 
