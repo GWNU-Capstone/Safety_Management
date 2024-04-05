10f79b09739603c5b0a833ae91263f4e4521e9c9
Chaewon <corndots@naver.com>
2024-04-06 01:05:30 +0900

    [Lee] 알코올, 체온/심박수

--- Back/src/main/java/com/application/safety/controller/UserDrinkController.java
+++ Back/src/main/java/com/application/safety/controller/UserDrinkController.java
@@ -1,33 +1,30 @@
 package com.application.safety.controller;
 
+
+import com.application.safety.dto.UserDataDTO;
 import com.application.safety.dto.UserProfileDTO;
 import com.application.safety.entity.UserData;
 import com.application.safety.entity.UserProfile;
 import com.application.safety.repository.UserDataRepository;
-import com.application.safety.repository.UserProfileRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// 음주
+
 @RestController
 @RequiredArgsConstructor
-public class MemberController {
-
-    // 사진(아직x), 이름, 사원번호
-    private final UserProfileRepository userProfileRepository;
+public class UserDrinkController {
 
     private final UserDataRepository userDataRepository;
 
-    @GetMapping("/members/{user_no}")
-    public UserProfileDTO getUserData(@PathVariable("user_no") int user_no) {
-        UserProfile userProfile = userProfileRepository.findById(user_no).orElseThrow(() -> new EntityNotFoundException("Not Found Id"));
-
-        UserProfileDTO dto = new UserProfileDTO();
-        dto.setUserNoPk(userProfile.getUserNoPk());
-        dto.setUserName(userProfile.getUserName());
-
+    @GetMapping("/members/{user_no}.drink")
+    public UserDataDTO getUserData(@PathVariable("user_no") int user_no) {
+        UserData userData = userDataRepository.findById(user_no).orElseThrow(() -> new EntityNotFoundException("Not Found Id"));
+        UserDataDTO dto = new UserDataDTO();
+        dto.setUserDrink(userData.getUserDrink());
 
         return dto;
 
@@ -36,4 +33,3 @@ public class MemberController {
 
 
 }
-
---
+++ Back/src/main/java/com/application/safety/dto/UserDataDTO.java
@@ -0,0 +1,15 @@
+package com.application.safety.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDataDTO {
+
+
+    private float UserDrink;
+    private float UserTemp;
+    private int UserHeartRate;
+
+}
