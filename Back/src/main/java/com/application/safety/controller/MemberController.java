faedf253d392263f4088925fdb6c2ff6cfddc8eb
taek2222 <oot309@daum.net>
2024-04-07 14:25:39 +0900

    [Oh] 사용자 (사원번호, 이름, 이미지) API 생성

--- Back/src/main/java/com/application/safety/controller/MemberController.java
+++ Back/src/main/java/com/application/safety/controller/MemberController.java
@@ -1,39 +1,46 @@
 package com.application.safety.controller;
 
+import com.application.safety.dto.UserInfoDTO;
 import com.application.safety.dto.UserProfileDTO;
-import com.application.safety.entity.UserData;
+import com.application.safety.entity.UserInfo;
 import com.application.safety.entity.UserProfile;
-import com.application.safety.repository.UserDataRepository;
+import com.application.safety.repository.UserInfoRepository;
 import com.application.safety.repository.UserProfileRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-
-    // 사진(아직x), 이름, 사원번호
     private final UserProfileRepository userProfileRepository;
-
-    private final UserDataRepository userDataRepository;
+    private final UserInfoRepository userInfoRepository;
 
     @GetMapping("/members/{user_no}")
-    public UserProfileDTO getUserData(@PathVariable("user_no") int user_no) {
-        UserProfile userProfile = userProfileRepository.findById(user_no).orElseThrow(() -> new EntityNotFoundException("Not Found Id"));
+    public ResponseEntity<Map<String, Object>> getUserData(@PathVariable("user_no") int user_no) {
 
-        UserProfileDTO dto = new UserProfileDTO();
-        dto.setUserNoPk(userProfile.getUserNoPk());
-        dto.setUserName(userProfile.getUserName());
+        // 사원 번호, 사용자 이름 DTO
+        UserProfile userProfile = userProfileRepository.findById(user_no).orElseThrow();
+        UserProfileDTO userProfileDTO = new UserProfileDTO();
+        userProfileDTO.setUserNoPk(userProfile.getUserNoPk());
+        userProfileDTO.setUserName(userProfile.getUserName());
 
+        // 사용자 사진 DTO
+        UserInfo userInfo = userInfoRepository.findById(user_no).orElseThrow();
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setUserImage(userInfo.getUserImage());
 
-        return dto;
+        // Map 활용으로 2개 DTO 합쳐서 반환
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("userProfile", userProfileDTO);
+        responseData.put("UserInfo", userInfoDTO);
 
+        return ResponseEntity.ok().body(responseData);
     }
-
-
-
 }
 
---
+++ Back/src/main/java/com/application/safety/dto/UserInfoDTO.java
@@ -0,0 +1,10 @@
+package com.application.safety.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDTO {
+    private String UserImage;
+}
