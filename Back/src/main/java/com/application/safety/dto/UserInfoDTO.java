717d13f4fbf4da31bc6546908519b470b48c0a21
taek2222 <oot309@daum.net>
2024-04-18 13:43:35 +0900

    [Oh] DTO 정리 및 추가

--- Back/src/main/java/com/application/safety/dto/UserInfoDTO.java
+++ Back/src/main/java/com/application/safety/dto/UserInfoDTO.java
@@ -6,5 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserInfoDTO {
+    private int UserNo;
     private String userImage;
+    private int UserAge;
+    private String UserTelNo;
 }
--- Back/src/main/java/com/application/safety/entity/UserInfo.java
+++ Back/src/main/java/com/application/safety/entity/UserInfo.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class UserInfo {
     @Id
     @Column(name="USER_NO")
