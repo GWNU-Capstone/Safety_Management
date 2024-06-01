8d5ee2e6bb455c1f81dfe3cc36c1acd0a8218f97
sapang123 <kim2061004@gmail.com>
2024-06-02 00:46:37 +0900

    api빼기

--- Front/src/components/Api.js
+++ Front/src/components/Api.js
@@ -1,2 +1,2 @@
 export const fingerprintApiBaseUrl = 'http://cshj020711.iptime.org:5050';  //소프트웨어 flask 서버 ip
-export const userApiBaseUrl = 'http://localhost:8080';         //서버 ip
+export const userApiBaseUrl = 'http://3.38.71.65';         //서버 ip
